5502113a3e59bb8eb1aeaeaa23c6820bbee9cccd
Eric Michelin <ericmichelin194@gmail.com>
2020-01-08 20:32:25 -0400

    added ledset command
    usage /ledset?=rgb

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from magichome import MagicHomeApi
 import time
 
 app = Flask(__name__)
 
 light_ip='192.168.2.12'
+
 
 @app.route('/')
 def index():
@@ -25,6 +26,13 @@ def ledoff():
 	controller1 = MagicHomeApi(light_ip, 0)
 	controller1.turn_off()
 	return 'Party Lights Off'
+
+@app.route('/ledset')
+def ledset():
+	controller1 = MagicHomeApi(light_ip, 0)
+	rgb = request.form['rgb']
+	controller1.update_device(rgb[0:2], rgb[3:5], rgb[6:8])
+	return 'Party Lights Set'
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
