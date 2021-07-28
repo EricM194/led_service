5fb8d26273abbb5d8cf44504e8ff5bdc5b948944
Eric <ericmichelin194@gmail.com>
2021-07-28 00:28:20 -0300

    get LED_IP form ENV var
    updated light_ip to LED_IP which is gather form the docker ENVs
    removed test /meow endpoint
    turned off debug mode

--- app.py
+++ app.py
@@ -1,52 +1,51 @@
 import magichue
+import os
 from flask import Flask, render_template, request
 
 # docs here: https://pypi.org/project/python-magichue/
 
 app = Flask(__name__)
 
-light_ip = '192.168.2.12'
-
+try:
+    led_ip = os.environ['LED_IP']
+except KeyError:
+    print('env variable "LED_IP" was not provided')
+    quit()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/meow')
-def meow():
-    return 'meow'
-
-
 @app.route('/ledon')
 def led():
-    magichue.Light(light_ip).on = True
-    return 'Party Lights On'
+    magichue.Light(led_ip).on = True
+    return 'LED Lights On'
 
 
 @app.route('/ledoff')
 def ledoff():
-    magichue.Light(light_ip).on = False
-    return 'Party Lights Off'
+    magichue.Light(led_ip).on = False
+    return 'LED Lights Off'
 
 
 @app.route('/ledset')
 def ledset():
     rgb = request.args.get('rgb')
     if len(rgb) == 9:
-        magichue.Light(light_ip).rgb = (int(rgb[0:2]), int(rgb[3:5]), int(rgb[6:8]))
-        return 'Party Lights Set'
+        magichue.Light(led_ip).rgb = (int(rgb[0:2]), int(rgb[3:5]), int(rgb[6:8]))
+        return 'LED Lights Set'
     if len(rgb) == 12:
-        magichue.Light(light_ip).rgb = (int(rgb[0:2]), int(rgb[3:5]), int(rgb[6:8]))
-        magichue.Light(light_ip).brightness = int(rgb[9:11]) * 10
-        return 'Party Lights Set With Brightness'
-    return 'Party Lights Not Set'
+        magichue.Light(led_ip).rgb = (int(rgb[0:2]), int(rgb[3:5]), int(rgb[6:8]))
+        magichue.Light(led_ip).brightness = int(rgb[9:11]) * 10
+        return 'LED Lights Set With Brightness'
+    return 'LED Lights Not Set'
 
 
 @app.route('/ledrainbow')
 def ledrainbow():
-    magichue.Light(light_ip).mode = magichue.RAINBOW_CROSSFADE
-    magichue.Light(light_ip).speed = 0.75
+    magichue.Light(led_ip).mode = magichue.RAINBOW_CROSSFADE
+    magichue.Light(led_ip).speed = 0.75
     return 'Rainbow'
 
 
@@ -57,4 +56,4 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
